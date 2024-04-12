dcfafcaa2b9f46f7d166c847384af03c7a434429
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-04-12 14:40:24 -0700

    Done with the lab.

--- Apps/PigLatin.Designer.cs
+++ Apps/PigLatin.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PigLatin";
+            exitButton = new Button();
+            translateButton = new Button();
+            groupBox2 = new GroupBox();
+            displayLabel = new Label();
+            groupBox1 = new GroupBox();
+            inputTextBox = new TextBox();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // exitButton
+            // 
+            exitButton.BackColor = Color.Tomato;
+            exitButton.ForeColor = SystemColors.ControlLightLight;
+            exitButton.Location = new Point(12, 180);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(441, 23);
+            exitButton.TabIndex = 7;
+            exitButton.Text = "EXIT";
+            exitButton.UseVisualStyleBackColor = false;
+            exitButton.Click += exitButtonClicked;
+            // 
+            // translateButton
+            // 
+            translateButton.BackColor = Color.ForestGreen;
+            translateButton.ForeColor = SystemColors.ControlLightLight;
+            translateButton.Location = new Point(12, 151);
+            translateButton.Name = "translateButton";
+            translateButton.Size = new Size(441, 23);
+            translateButton.TabIndex = 6;
+            translateButton.Text = "Translate";
+            translateButton.UseVisualStyleBackColor = false;
+            translateButton.Click += translateButtonClicked;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(displayLabel);
+            groupBox2.Location = new Point(12, 85);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(441, 60);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Pig Latin";
+            // 
+            // displayLabel
+            // 
+            displayLabel.BackColor = SystemColors.ControlLight;
+            displayLabel.Location = new Point(6, 19);
+            displayLabel.Name = "displayLabel";
+            displayLabel.Size = new Size(429, 34);
+            displayLabel.TabIndex = 0;
+            displayLabel.Text = "I can see you";
+            displayLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(inputTextBox);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(441, 67);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Enter Your Text:";
+            // 
+            // inputTextBox
+            // 
+            inputTextBox.Location = new Point(5, 27);
+            inputTextBox.Name = "inputTextBox";
+            inputTextBox.Size = new Size(429, 23);
+            inputTextBox.TabIndex = 0;
+            // 
+            // PigLatin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(462, 215);
+            Controls.Add(exitButton);
+            Controls.Add(translateButton);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "PigLatin";
+            Text = "PigLatin";
+            groupBox2.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button exitButton;
+        private Button translateButton;
+        private GroupBox groupBox2;
+        private Label displayLabel;
+        private GroupBox groupBox1;
+        private TextBox inputTextBox;
     }
 }
--- Apps/PigLatin.cs
+++ Apps/PigLatin.cs
@@ -1,20 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CS162_Lab2.Apps
+﻿namespace CS162_Lab2.Apps
 {
     public partial class PigLatin : Form
     {
         public PigLatin()
         {
             InitializeComponent();
+        }
+
+        private void exitButtonClicked(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void translateButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string inputString = inputTextBox.Text;
+                string[] inputSplit = inputString.Split(null);
+
+                string newString = "";
+
+                foreach (var stringSplit in inputSplit)
+                {
+                    char[] tokenalizedString = stringSplit.ToCharArray();
+
+                    char firstChar = tokenalizedString[0];
+
+                    foreach (var charItem in tokenalizedString)
+                    {
+                        if(charItem == firstChar) { continue; }
+                        newString += char.ToUpper(charItem);
+                    }
+                    newString = newString + $"{char.ToUpper(firstChar)}AY ";
+
+                }
+
+                displayLabel.Text = newString;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
